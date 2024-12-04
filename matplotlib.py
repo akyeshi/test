600d73659307cef000e40cf69a4450c58fd4039b
Yeshi <e467739@us.lmco.com>
2024-12-04 18:43:25 -0500

    matplotlib pyplot module

---
+++ matplotlib.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.972]
+plt.scatter(year, pop)  # display collection of points on a grid corresponding to x/y values 
+plt.show() 
+
+
+import matplotlib.pyplot as plt 
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.972]
+plt.plot(year, pop)     # series of points connected by a line chart 
+plt.show() 
